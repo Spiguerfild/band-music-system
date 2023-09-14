69857c6d79ab021e2d8af09d805a6ffe3c1f8321
Spiguerfild <luique16@outlook.com>
2023-09-13 22:38:41 -0400

    update p abrir a fronts o back

--- src/main/java/br/com/music/BandMusicSystemApplication.java
+++ src/main/java/br/com/music/BandMusicSystemApplication.java
@@ -2,10 +2,20 @@ package br.com.music;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
-public class BandMusicSystemApplication {
+@EnableWebMvc
+public class BandMusicSystemApplication implements WebMvcConfigurer {
 
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS");
+	}
+	
+	
 	public static void main(String[] args) {
 		SpringApplication.run(BandMusicSystemApplication.class, args);
 	}
